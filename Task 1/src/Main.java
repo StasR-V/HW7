0ea1e32bd938567c05557da61a415dd844bb3f61
Stanislav R <stas_sapsan13@mail.ru>
2025-05-15 02:42:40 +0700

    Дописал код

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
         int ghoulTearsCount = 0;        //Слезы вурдалака
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
-
+        boolean isTrue = false;
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("Количество ингредиента 'Жабьи глаза'");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.println("Количество ингредиента 'Слёзы вурдалака'");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+        System.out.println("Количество ингредиента 'Кости ворона'");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.println("Количество ингредиента 'Пельмени'");
+        dumplingsCount = new Scanner(System.in).nextInt();
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -14,5 +24,25 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+        if (toadEyesCount >= 3 && ravenBonesCount >= 1) {
+            System.out.println("Элексир зоркости");
+            isTrue = true;
+        }
+        if (ravenBonesCount > 1 && dumplingsCount >= 4) {
+            System.out.println("Элексир стойкости");
+            isTrue = true;
+        }
+        if ((toadEyesCount > 1 && dumplingsCount >= 1) && ghoulTearsCount >= 7) {
+            System.out.println("Элексир скрытности");
+            isTrue = true;
+        }
+        if ((toadEyesCount >= 4 && dumplingsCount >= 10) && (ghoulTearsCount >= 5 && ravenBonesCount >= 3)) {
+            System.out.println("Элексир запретный");
+            isTrue = true;
+        }
+        if (isTrue == false){
+            System.out.println("У вас не хватает ингредиентов ни на один элексир. Доставайте оружие из широких штанин" +
+                    " и чешите на данж!");
+        }
     }
 }
