2f542247d8799d34d31a62e0b9c4899c0597da5d
Stanislav R <stas_sapsan13@mail.ru>
2025-05-15 03:04:59 +0700

    Уволил айтишников с помощью FOR.

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+        //todo Тут увольняем айтишников(Каждого второго)
+        int numberOfEmployees = 100;
+        int idPerson = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                idPerson = idPerson + 2;
+                System.out.println("Уволен айтишник с ID " + idPerson);
+            }
+        }
     }
 }
